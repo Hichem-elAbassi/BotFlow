9d69000c8eec7a48179984d515047855455d7d32
ArTech Engineering <artech.professionals@gmail.com>
2020-08-01 02:22:02 +0100

    added branches

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
-
+/** API functions **/
+/*
+1. CRUD on "element"
+*/
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var results={
+    "name":"hichem",
+    "function":"testing"
+  }
+  res.json(results);
 });
-
+router.get('/elements', function(req, res, next) {
+  var results={
+    elements:[{"name":"test 1","function":"vector"},
+              {"name":"test 2","function":"vector"},
+              {"name":"test 3","function":"vector"},
+              {"name":"test 4","function":"vector"}]
+  }
+  res.json(results);
+});
+router.post('/elements', function(req, res, next) {
+  var results={
+    "name":"hichem",
+    "function":"testing"
+  }
+  res.json(results);
+});
+router.get('/', function(req, res, next) {
+  var results={
+    "name":"hichem",
+    "function":"testing"
+  }
+  res.json(results);
+});
 module.exports = router;
